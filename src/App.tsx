f3cd088be044d04df126648cec7bd30c9224e77a
svan23 <vansla>
2025-11-01 20:09:46 -0700

    Add authentication protection for routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,41 @@ import Summery from "./components/uploader/Summery";
 import UploaderWrapper from "./components/uploader/UploaderWrapper";
 import SummeryList from "./components/SummeryList";
 import AdminDashboard from "./components/AdminDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
-     <Routes>
-      <Route element={<Layout />}>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<UploaderWrapper />} />
-        <Route path="/summery" element={<SummeryList />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <UploaderWrapper />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/summery"
+            element={
+              <ProtectedRoute>
+                <SummeryList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
     </>
   );
 }
